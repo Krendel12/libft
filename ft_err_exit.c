f6e3bbbe2b114aaa0d2a2f839bcaf5a630e64a10
Unknown <krendelllllll@gmail.com>
2018-08-08 14:32:02 +0300

    Change err_exit to print message in red color

--- ft_err_exit.c
+++ ft_err_exit.c
@@ -6,7 +6,7 @@
 /*   By: astrelov <astrelov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/08 13:38:45 by astrelov          #+#    #+#             */
-/*   Updated: 2018/08/08 13:38:45 by astrelov         ###   ########.fr       */
+/*   Updated: 2018/08/08 14:29:21 by astrelov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,10 @@
 int		ft_err_exit(char *err_msg)
 {
 	if (err_msg)
+	{
+		ft_printf(COLOR_BRED);
 		ft_putendl_fd(err_msg, 2);
+		ft_printf(COLOR_RESET);
+	}
 	exit(1);
 }
